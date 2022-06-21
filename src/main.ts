7e43d2a88e217b6a51f516581d041a864ecc403e
Arturo Estrada <arturo.estrada@ipointsystems.com>
2022-06-20 20:11:22 -0500

    feat: initial firebase nestjs integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import * as express from 'express';
 import { AppModule } from './app.module';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const server = express();
+
+export const createNestServer = async (expreessInstance: express.Express) => {
+  const adapter = new ExpressAdapter(expreessInstance);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    adapter,
+    {},
+  );
+
+  app.enableCors();
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest Failed', err));
+
+export const api = functions.https.onRequest(server);
